d42f2d334a57be104ef4efbe2062fa4bcc0b1a12
Julija Petrova <jule.p3t@gmail.com>
2017-03-24 11:03:40 +0100

    Add project files.

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+using System.Net.Mail;
+
+namespace AarhusWebDevCoop.Controllers
+{
+
+
+   
+
+   
+
+
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["success"] = true;
+                return CurrentUmbracoPage();
+            }
+
+
+            /*create new documents based on the form*/
+            IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "Message");
+            comment.SetValue("messagename", model.Name);
+            comment.SetValue("email", model.Email);
+            comment.SetValue("subject", model.Subject);
+            comment.SetValue("messagecontent", model.Message);
+
+            Services.ContentService.SaveAndPublishWithStatus(comment);
+            //Services.ContentService.Save(comment);
+
+            /*create new mail to send based on the form*/
+            MailMessage message = new MailMessage();
+            message.To.Add("jule.p3t@gmail.com");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("juletheforumtroll@gmail.com", "CocaCola2017");
+                smtp.EnableSsl = true;
+
+                //send mail
+                smtp.Send(message);
+
+            }
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ AarhusWebDevCoop/ViewModels/ContactForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AarhusWebDevCoop.ViewModels
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Please add a name. This field can't be empty.")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage ="This is not a valid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please add a subject. This field can't be empty.")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please add a message. This field can't be empty.")]
+        public string Message { get; set; }
+    }
+}
